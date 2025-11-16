b8ad150e773a49c253463ba6d31ec6d8cd549ad9
stail/claude <dmwattem@gmail.com>
2025-11-15 21:10:55 -0500

    test 2

--- gallery.java
+++ gallery.java
@@ -5,6 +5,19 @@ class Gallery{
         String directoryPath = "testDirectory";
         File directory = new File(directoryPath);
 
+
+
+        if(directory.exists()&& directory.isDirectory()){
+            String[] sontents = directory.list();
+            if (contents != null){
+                for (String content : contents){
+                    System.out.println(content)
+                }
+            }
+        }
+
+
+
     }
 
 }
